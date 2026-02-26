9982fa3df52850b45f1bab46f3444891bea34782
msriakhila999 <msriakhila999@gmail.com>
2026-02-26 15:31:51 +0530

    Akhila added UC-2

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,13 +1,37 @@
 public class PalindroneCheckerApp {
 
+    /**
+     * Application entry point for UC2.
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
 
-        System.out.println("=========================================");
-        System.out.println(" Welcome to Palindrome Checker System ");
-        System.out.println("=========================================");
-        System.out.println("Application Version: 1.6");
-        System.out.println("System started successfully.");
-        System.out.println("=========================================");
 
+        String word = "madam";
+
+
+        word = word.toLowerCase();
+
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = word.length() - 1;
+
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is NOT a palindrome.");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PalindroneCheckerApp
+p
+
+   
